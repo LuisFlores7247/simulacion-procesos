a0e91e242c2f4c3176364d20b0b426fb35dde19d
Luis Flores <lflores2719@ags.conalep.edu.mx>
2023-11-16 09:59:46 -0600

    Avance Buddy System

---
+++ Lista.cpp
@@ -0,0 +1,65 @@
+#include "Lista.h"
+
+// Constructor de la clase Lista
+Lista::Lista()
+{
+    inicio = fin = NULL; // Inicializa los apuntadores
+}
+
+// Metodos de la clase Lista
+int Lista::nuevoProceso(int _id, int _tamanio, int _cuanto)
+{
+    Proceso *aux = new Proceso(_id, _tamanio, _cuanto); // Crea un nuevo nodo
+    if (_id != 0 && _cuanto != 0)
+    {
+        fin = aux; // El nuevo nodo es el fin de la lista
+    }
+    else
+    {
+        this->buddySystem(aux);
+        if (inicio == NULL)
+        {
+            inicio = aux; // Si la lista está vacía, el nuevo nodo es el inicio y el fin
+        }
+        else
+        {
+            fin->liga = aux; // Si no, el nuevo nodo se liga al final de la lista
+        }
+    }
+
+    return aux->tamanio;
+}
+
+void Lista::buddySystem(Proceso *_proceso)
+{
+    Proceso *aux = inicio;
+
+    while (aux != NULL && aux->tamanio >= 64)
+    {
+        if (aux->tamanio >= (_proceso->tamanio * 2))
+        {
+            this->nuevoProceso(0, aux->tamanio / 2, 0);
+            aux->tamanio /= 2;
+        }
+        aux = aux->liga;
+    }
+}
+
+void Lista::imprimir()
+{
+    cout << endl;
+    if (inicio == NULL)
+    {
+        cout << "Lista Vacia..." << endl;
+    }
+    else
+    {
+        Proceso *aux = inicio;
+        while (aux != NULL)
+        {
+            cout << "[" << aux->id << "," << aux->tamanio << "," << aux->cuanto << "," << aux->mem_asignada << "]";
+            aux = aux->liga;
+        }
+    }
+    cout << endl;
+}
---
+++ Lista.h
@@ -0,0 +1,29 @@
+#ifndef LISTA_H
+#define LISTA_H
+
+#include <iostream>
+#include "Proceso.cpp"
+
+using namespace std;
+
+enum TAM_MEMORIA
+{
+    TAM_1Mb = 1024,
+    TAM_4Mb = 4096,
+    TAM_8Mb = 8192
+};
+
+class Lista
+{
+public:
+    Lista();
+    int nuevoProceso(int, int, int);
+    void buddySystem(Proceso *);
+    void imprimir();
+
+private:
+    Proceso *inicio;
+    Proceso *fin;
+};
+
+#endif
